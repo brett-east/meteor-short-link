2c3c4967351129bd1e47235a6b52384165ae8ea3
Brett East <brett@sproutback.com>
2017-06-05 20:43:51 +1000

    Add Hide and Unhide button and visibility property of links

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
@@ -27,8 +28,14 @@ export default class LinksListItem extends React.Component {
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <p>{this.props.visible.toString()}</p>
         <button ref="copy" data-clipboard-text={this.props.shortUrl}>
           {this.state.justCopied ? 'copied' : 'copy'}
+        </button>
+        <button onClick={() => {
+          Meteor.call('links.setVisibility', this.props._id, !this.props.visible);
+        }}>
+          {this.props.visible ? 'Hide' : 'Unhide'}
         </button>
       </div>
     );
@@ -39,5 +46,6 @@ LinksListItem.propTypes = {
   _id: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   shortUrl: PropTypes.string.isRequired,
+  visible: PropTypes.bool.isRequired,
   userId: PropTypes.string.isRequired
 };
