8b35bd3da5216ee3ed8990f68afe0f1c1bec6436
Brett East <brett@sproutback.com>
2017-06-10 11:06:54 +1000

    Add lastVisitedAt and visitedCount to links

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -29,6 +29,7 @@ export default class LinksListItem extends React.Component {
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
         <p>{this.props.visible.toString()}</p>
+        <p>{this.props.visitedCount} - {this.props.lastVisitedAt}</p>
         <button ref="copy" data-clipboard-text={this.props.shortUrl}>
           {this.state.justCopied ? 'copied' : 'copy'}
         </button>
@@ -47,5 +48,7 @@ LinksListItem.propTypes = {
   url: PropTypes.string.isRequired,
   shortUrl: PropTypes.string.isRequired,
   visible: PropTypes.bool.isRequired,
-  userId: PropTypes.string.isRequired
+  userId: PropTypes.string.isRequired,
+  visitedCount: PropTypes.number.isRequired,
+  lastVisitedAt: PropTypes.number
 };
